64db2ab1c373fdab8295b3b2f63d48e34618c42b
cRivera <riverabravoc@outlook.com>
2023-02-23 21:14:53 -0300

    Revert "Merge branch 'main' of https://github.com/repotmn/tumedianaranja_crm"
    This reverts commit 352c9d44fb714527d8de5e2c2248a32956d97342, reversing
    changes made to aab24314620cdf45c913f39272404d56eac83db4.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,28 +1,50 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import Logo from '../assets/Logo.png';
+import '../css/login.css';
 
 function Login() {
 
   return (
-        <div className="vh-100 d-flex justify-content-center align-items-center">
-            <div className="col-md-4 p-5 capsula rounded-3">
-                <h2 className="text-center mb-4 text-primary">Login</h2>
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1"/>
-                    </div>
-                    <p className="small"><a className="text-primary">Olvidaste tu contraseña?</a></p>
-                    <div className="d-grid">
-                        <Link className ="btn btn-primary" to="/prospeccion">Ingresar</Link>
-                    </div>
-                </form>
+    <section className="vh-100 gradient-custom">
+  <div className="container py-5 h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div className="card bg-dark text-white" style={{borderRadius: "1rem"}}>
+          <div className="card-body p-5 text-center">
+
+            <div className="mb-md-5 mt-md-4 pb-5">
+              <h2><img src={Logo} alt=""/></h2>
+
+              <div className="form-outline form-white mb-4">
+                <input type="email" id="typeEmailX" className="form-control form-control-lg" />
+                <label className="form-label" htmlFor="typeEmailX">Usuario</label>
+              </div>
+
+              <div className="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                <label className="form-label" htmlFor="typePasswordX">Password</label>
+              </div>
+              <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Olvidaste tu password?</a></p>
+
+              <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+              <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                <a href="#!" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
+                <a href="#!" className="text-white"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                <a href="#!" className="text-white"><i className="fab fa-google fa-lg"></i></a>
+              </div>
+
             </div>
+            <div>
+              <p className="mb-0">No tienes una cuenta? <a href="#!" className="text-white-50 fw-bold">Registrarse</a>
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+  </div>
+</section>
   );
 }
 export default Login
